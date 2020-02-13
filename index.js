5f55b32d22c93405cce3b473309d062b24ab3a03
Jessica Wolff <jleahwolff@gmail.com>
2020-02-13 12:01:40 -0600

    big bang

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("\n ***index.js ran*** \n");
+
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+    const songs = [{ id: 1, name: "Final Countdown" }];
+
+    res.status(200).json(songs);
+});
+
+const port = 5000;
+server.listen(port, () => console.log(`\n** Running on port ${port}`))
